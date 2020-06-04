bac6d50d192a0632cd6447e5038eeff27983bb47
unknown <vredes@live.com>
2020-06-03 23:34:20 -0400

    Async pipes using HttpClient & HttpParams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import {COURSES} from '../db-data';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
+import {Observable} from 'rxjs'; //Import in order to use an Observable
+import { HttpClient, HttpParams } from '@angular/common/http'; //Import in order to use http method with params
 
 @Component({
   selector: 'app-root',
@@ -12,16 +13,22 @@ import {Observable} from 'rxjs';
 })
 export class AppComponent implements OnInit {
 
+  //Declare an observable variableName$ (courses$) with its : Type (: Observable) and <value>(<Course[]>)
+  courses$: Observable<Course[]>;
 
-  courses = COURSES;
-
-  constructor() {
+  //Convert the imported HttpClient method in the constructor
+  constructor(private http: HttpClient) {
 
   }
 
   ngOnInit() {
+
+    const params = new HttpParams()
+      .set("page", "1")
+      .set("pageSize", "10");
+    
+
+    this.courses$ = this.http.get<Course[]>('/api/courses', {params});
   }
-
-
 
 }
